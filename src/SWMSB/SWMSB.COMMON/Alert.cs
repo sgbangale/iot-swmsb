e11a8a424789e43727b658c99acb9d34fd3cc435
Suraj Bangale <sgbangale@gmail.com>
2020-09-21 17:42:52 +0530

    updated missing packages

--- src/SWMSB/SWMSB.COMMON/Alert.cs
+++ src/SWMSB/SWMSB.COMMON/Alert.cs
@@ -1,9 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace SWMSB.COMMON
 {
