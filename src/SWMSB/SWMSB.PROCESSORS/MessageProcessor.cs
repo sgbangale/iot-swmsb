7f8ea84e7452e2d36b64c6d9ead03f84bde18952
Suraj Bangale <sgbangale@gmail.com>
2020-09-22 23:25:19 +0530

    fn changes

--- src/SWMSB/SWMSB.PROCESSORS/MessageProcessor.cs
+++ src/SWMSB/SWMSB.PROCESSORS/MessageProcessor.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SWMSB.BAL;
 using SWMSB.COMMON;
 using System;
@@ -30,42 +31,54 @@ namespace SWMSB.PROCESSORS
             var exceptions = new List<Exception>();
             BackendRepository backendRepository = new BackendRepository(Config, log);
             log.LogInformation($"{typeof(MessageProcessor)} event count-: {events?.Length}");
-            foreach (EventData eventData in events)
+            foreach (var eventData in events)
             {
+                JArray jsonArray = new JArray();
+                string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
                 try
                 {
-                    string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
-
-                    var alert = JsonConvert.DeserializeObject<Alert>(messageBody);
-
-                    var alertValidationResult = alert.Validate();
-                    if (alertValidationResult.Success)
-                    {
-                        switch (alert.AlertType)
-                        {
-                            case AlertType.ALERT_FIFTY_G:
-                                await backendRepository.SendLeakEmailAsync(alert);
-                                break;
-                            case AlertType.ALERT_THOUSAND_G:
-                                await backendRepository.SendUsageExhaustEmailAsync(alert);
-                                break;
-                            default:
-                                break;
-                        }
-                        log.LogInformation($"{typeof(MessageProcessor)} processed a message: {alert.ToIntendedJsonString()}");
-
-                    }
-                    else
-                    {
-                        log.LogError($"{typeof(MessageProcessor)} model validation failed-{alertValidationResult.ErrorMessage}: {alert.ToIntendedJsonString()}");
-                    }
-
-                    await Task.Yield();
+                    jsonArray = JArray.Parse(messageBody);
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    exceptions.Add(e);
-                    log.LogError(e, $"{typeof(MessageProcessor)} exception-{e.Message}");
+                    jsonArray.Add(JsonConvert.DeserializeObject(messageBody));
+                }
+
+                foreach (var eventJson in jsonArray)
+                {
+                    try
+                    {
+                        var alert = JsonConvert.DeserializeObject<Alert>(eventJson.ToString());
+
+                        var alertValidationResult = alert.Validate();
+                        if (alertValidationResult.Success)
+                        {
+                            switch (alert.AlertType)
+                            {
+                                case AlertType.ALERT_FIFTY_G:
+                                    await backendRepository.SendLeakEmailAsync(alert);
+                                    break;
+                                case AlertType.ALERT_THOUSAND_G:
+                                    await backendRepository.SendUsageExhaustEmailAsync(alert);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            log.LogInformation($"{typeof(MessageProcessor)} processed a message: {alert.ToIntendedJsonString()}");
+
+                        }
+                        else
+                        {
+                            log.LogError($"{typeof(MessageProcessor)} model validation failed-{alertValidationResult.ErrorMessage}: {alert.ToIntendedJsonString()}");
+                        }
+
+                        await Task.Yield();
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                        log.LogError(e, $"{typeof(MessageProcessor)} exception-{e.Message}");
+                    }
                 }
             }
         }
