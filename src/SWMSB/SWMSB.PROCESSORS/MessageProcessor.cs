4f0698ee906292cab6b7bf2da5d3c8299b0df211
Suraj Bangale <sgbangale@gmail.com>
2020-09-22 17:21:03 +0530

    changes for routing and email bug fix

--- src/SWMSB/SWMSB.PROCESSORS/MessageProcessor.cs
+++ src/SWMSB/SWMSB.PROCESSORS/MessageProcessor.cs
@@ -36,37 +36,36 @@ namespace SWMSB.PROCESSORS
                 {
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
-                    var _alerts = JsonConvert.DeserializeObject<Alert[]>(messageBody);
-                    foreach (var alert in _alerts)
-                    {
-                        var alertValidationResult = alert.Validate();
-                        if (alertValidationResult.Success)
-                        {
-                            switch (alert.AlertType)
-                            {
-                                case AlertType.ALERT_FIFTY_G:
-                                    await backendRepository.SendLeakEmailAsync(alert);
-                                    break;
-                                case AlertType.ALERT_THOUSAND_G:
-                                    await backendRepository.SendUsageExhaustEmailAsync(alert);
-                                    break;
-                                default:
-                                    break;
-                            }
-                            log.LogInformation($"{typeof(MessageProcessor)} processed a message: {alert.ToIntendedJsonString()}");
+                    var alert = JsonConvert.DeserializeObject<Alert>(messageBody);
 
-                        }
-                        else
+                    var alertValidationResult = alert.Validate();
+                    if (alertValidationResult.Success)
+                    {
+                        switch (alert.AlertType)
                         {
-                            log.LogError($"{typeof(MessageProcessor)} model validation failed-{alertValidationResult.ErrorMessage}: {alert.ToIntendedJsonString()}");
+                            case AlertType.ALERT_FIFTY_G:
+                                await backendRepository.SendLeakEmailAsync(alert);
+                                break;
+                            case AlertType.ALERT_THOUSAND_G:
+                                await backendRepository.SendUsageExhaustEmailAsync(alert);
+                                break;
+                            default:
+                                break;
                         }
+                        log.LogInformation($"{typeof(MessageProcessor)} processed a message: {alert.ToIntendedJsonString()}");
+
                     }
+                    else
+                    {
+                        log.LogError($"{typeof(MessageProcessor)} model validation failed-{alertValidationResult.ErrorMessage}: {alert.ToIntendedJsonString()}");
+                    }
+
                     await Task.Yield();
                 }
                 catch (Exception e)
-                { 
+                {
                     exceptions.Add(e);
-                    log.LogError($"{typeof(MessageProcessor)} exception:", e.Message);
+                    log.LogError(e, $"{typeof(MessageProcessor)} exception");
                 }
             }
         }
